578fd1cb60c223518b4d20bfb2c1acfa54cdda7e
Jotham Teshome <jothamteshome@gmail.com>
2024-08-07 19:48:14 -0400

    Added function to handle full training loop of model

--- utils/train.py
+++ utils/train.py
@@ -5,6 +5,7 @@ from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from utils.evaluate import evaluateEpoch
 from utils.models import ImageCaptioningNetwork
 
 
@@ -13,6 +14,7 @@ def trainEpoch(model: ImageCaptioningNetwork, optimizer: AdamW, train_loader: Da
     
     Handles training a model for a single epoch
 
+    
     Parameters:
         model (ImageCaptioningNetwork):     Model to train on dataset for one epoch
         optimizer (AdamW):                  Optimizer to adjust parameters during training
@@ -20,6 +22,7 @@ def trainEpoch(model: ImageCaptioningNetwork, optimizer: AdamW, train_loader: Da
         device (device):                    Device to copy data to while training
         pad_idx (int):                      ID representing padding token
 
+        
     Returns:
         float:      A float value representing the evaluation loss at the current training step
 
@@ -60,3 +63,37 @@ def trainEpoch(model: ImageCaptioningNetwork, optimizer: AdamW, train_loader: Da
         optimizer.step()
 
     return running_loss / len(train_loader)
+
+
+def trainModel(model: ImageCaptioningNetwork, train_loader: DataLoader, val_loader: DataLoader, device: device, pad_idx: int, num_epochs:int=5) -> None:
+    """
+    
+    Runs full training loop of model for given amount of epochs
+
+
+    Parameters:
+        model (ImageCaptioningNetwork):     Model to train on dataset for one epoch
+        train_loader (DataLoader):          DataLoader contining training data to train model on
+        val_loader (DataLoader):            DataLoader contining validation data to evaluate model on
+        device (device):                    Device to copy data to while training
+        pad_idx (int):                      ID representing padding token
+    
+    """
+
+    # Initialize optimizer for updating model parameters
+    optimizer = AdamW(model.parameters(), lr=1e-4, weight_decay=1e-4)
+
+    # Run for set number of epochs
+    for epoch in range(num_epochs):
+
+        # Train model for current epoch and get training loss
+        train_loss = trainEpoch(model, optimizer, train_loader, device, pad_idx)
+
+        # Save checkpoint after epoch model
+        torch.save(model.state_dict(), f"google_model_checkpoints/checkpoint_epoch_{epoch+1}.pt")
+
+        # Evaluate model for current epoch and get validation loss
+        val_loss = evaluateEpoch(model, val_loader, device, pad_idx)
+
+        # Print current epoch and loss metrics for training and testing
+        print(f"Epoch {epoch+1} | train_loss: {train_loss} | val_loss: {val_loss}")
