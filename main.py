9e928cffba54182cab6c374c8ad28fc9ee732fb5
Jotham Teshome <jothamteshome@gmail.com>
2024-08-08 04:19:00 -0400

    Main script to begin training model

--- main.py
+++ main.py
@@ -1,115 +1,19 @@
-import os
-import torch
-
-from torch import nn, optim
-from tqdm import tqdm
-
-
-from get_dataloaders import getTrainLoaders, getWord2VecEmbeddings
-from get_models import ImageCaptioningNetwork
-
-# Handles training step for a single epoch
-def train_epoch(model, optimizer, train_loader, device):
-    # Initialize loss function
-    criterion = nn.CrossEntropyLoss()
-
-    # Set model to training mode and initialize loss
-    model.train()
-    running_loss = 0
-
-
-    # Train on all batches of training dataloader
-    for images, captions in tqdm(train_loader, unit='batch', desc='Training'):
-        optimizer.zero_grad()
-
-        # Move data to device
-        images = images.to(device)
-        captions = captions.to(device)
-
-        # Compute predictions and loss
-        outputs = model(images, captions)[:, 1:, :]
-
-        # Reshape data in format used by criterion
-        outputs = outputs.reshape(outputs.shape[0] * outputs.shape[1], outputs.shape[2])
-        captions = captions.view(captions.shape[0] * captions.shape[1])
-
-
-        # First position comes from concatenating features with embeddings
-        loss = criterion(outputs, captions)
-        running_loss += loss.item()
-
-        # Handle backpropagation
-        loss.backward()
-        optimizer.step()
-
-    return running_loss / len(train_loader)
-
-
-# Function that runs the evaluation step of the model
-def evaluate_model(model, dataloader, device):
-    # Set model to evaluation mode and initialize loss
-    model.eval()
-    running_loss = 0
-
-    # Initialize loss function
-    criterion = nn.CrossEntropyLoss()
-
-    with torch.no_grad():
-        # Evaluate all batches of dataloader
-        for images, captions in tqdm(dataloader, unit='batch', desc='Evaluating'):
-            # Move data to device
-            images = images.to(device)
-            captions = captions.to(device)
-
-            # Compute predictions and loss
-            outputs = model(images, captions)[:, 1:, :]
-
-            # Reshape data in format used by criterion
-            outputs = outputs.reshape(outputs.shape[0] * outputs.shape[1], outputs.shape[2])
-            captions = captions.view(captions.shape[0] * captions.shape[1])
-
-            loss = criterion(outputs, captions)
-            running_loss += loss.item()
-
-
-    return running_loss / len(dataloader)
-
-
-def train_model(model, train_loader, val_loader, device, num_epochs=5):
-    optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
-
-    os.makedirs('model_checkpoints', exist_ok=True)
-
-    for epoch in range(num_epochs):
-        train_loss = train_epoch(model, optimizer, train_loader, device)
-
-        # Save checkpoint after epoch model
-        torch.save(model.state_dict(), f"model_checkpoints/checkpoint_epoch_{epoch+1}.pt")
-
-        val_loss = evaluate_model(model, val_loader, device)
-
-        print(f"Epoch {epoch+1} | train_loss: {train_loss} | val_loss: {val_loss}")
+from utils.data import getTrainLoaders
+from utils.models import loadModel
+from utils.train import trainModel, TrainingArgs
 
 
 def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_args = TrainingArgs()
 
+    # Load dataset
+    train_loader, val_loader = getTrainLoaders()
 
-    word2vec, caption_length = getWord2VecEmbeddings()
-    train_loader, val_loader = getTrainLoaders(word2vec, caption_length)
+    # Load pretrained model if path is passed in, otherwise load new model
+    model = loadModel().to(train_args.device)
 
-    model = ImageCaptioningNetwork(embedding_dim=128,
-                                   num_embeddings=len(word2vec.wv.key_to_index),
-                                   hidden_size=128).to(device)
-
-
-    train_model(model, train_loader, val_loader, device)
-
-    # Save trained model
-    torch.save(model.state_dict(), f"image_captioning_model.pt")
-
-
-
+    # Run training loop for model
+    trainModel(model, train_loader, val_loader, train_args)
 
 if __name__ == "__main__":
     main()
