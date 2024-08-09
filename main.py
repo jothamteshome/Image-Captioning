912b6c17bc219edd201adb2b0bf65602e56906b2
Jotham Teshome <jothamteshome@gmail.com>
2024-08-09 01:14:30 -0400

    Modified main training pipeline to include BLEU score computation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from utils.data import getTrainLoaders
+from utils.data import getTrainLoaders, getFinalEvalLoader
+from utils.evaluate import BLEUEval
 from utils.models import loadModel
 from utils.train import trainModel, TrainingArgs
 
+def train():
+    # Initialize training arguments
+    train_args = TrainingArgs(num_epochs=7, learning_rate=5e-4)
 
-def main():
-    train_args = TrainingArgs()
-
-    # Load dataset
+    # Load training and validation data
     train_loader, val_loader = getTrainLoaders()
 
     # Load pretrained model if path is passed in, otherwise load new model
@@ -15,5 +16,11 @@ def main():
     # Run training loop for model
     trainModel(model, train_loader, val_loader, train_args)
 
+    # Compute BLEU score on trained model
+    bleu_loader = getFinalEvalLoader()
+    bleu_score = BLEUEval(model, bleu_loader, train_args.device)
+    train_args.saveMetrics({'bleu_score': bleu_score})
+
+
 if __name__ == "__main__":
-    main()
+    train()
